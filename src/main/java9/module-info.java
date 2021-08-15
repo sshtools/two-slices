b39f3e97da84d979802834de3abe49860e6f8b48
brett-smith <brett@hypersocket.com>
2021-08-15 16:35:34 +0100

    Another attempt.

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -3,8 +3,8 @@ module com.sshtools.twoslices {
 	requires static java.scripting;
 	requires static org.freedesktop.dbus;
 	requires static org.eclipse.swt.gtk.linux.x86_64;
-	requires static org.controlsfx.control;
 	requires static javafx.controls;
 	requires static javafx.graphics;
+	requires static transitive org.controlsfx.controls;
 	exports com.sshtools.twoslices;
 }

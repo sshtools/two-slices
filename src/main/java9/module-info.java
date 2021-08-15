841a05a071cd990fb7300efcfcad498b121a4568
brett-smith <brett@hypersocket.com>
2021-08-15 16:33:28 +0100

    Just this?

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -4,7 +4,6 @@ module com.sshtools.twoslices {
 	requires static org.freedesktop.dbus;
 	requires static org.eclipse.swt.gtk.linux.x86_64;
 	requires static org.controlsfx.control;
-	requires static org.controlsfx.tools;
 	requires static javafx.controls;
 	requires static javafx.graphics;
 	exports com.sshtools.twoslices;
